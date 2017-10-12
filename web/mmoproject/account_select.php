8f695afc158c559dd8ce842415e823fd6e865c7c
bdaxlol <dax.guthrie@gmail.com>
2017-10-12 15:59:49 -0700

    added db connection to account_select.php

--- web/mmoproject/account_select.php
+++ web/mmoproject/account_select.php
@@ -1,6 +1,29 @@
 <?php
-require "dbConnect.php"
-$db = get_db();
+	$db = NULL;
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			$dbUrl = "postgres://postgres:mniDnb16@localhost:5432/postgres";
+		}
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
 ?>
 
 <!DOCTYPE html>
