6cd4e4bc61820f1098ebfe59c4875adea72674f1
bdaxlol <dax.guthrie@gmail.com>
2017-10-19 16:58:38 -0700

    added loggin to index.js

--- web/mmoproject/index.js
+++ web/mmoproject/index.js
@@ -1,4 +1,5 @@
 function setSession(var, val) {
+	console.log("setSession was called. Parameters are " + var + " & " + val);
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
