fce28d62fcd345db6703d6fa712643e2ffb9f4c2
bdaxlol <dax.guthrie@gmail.com>
2017-10-20 13:28:48 -0700

    fixed cap error in index.js

--- web/mmoproject/index.js
+++ web/mmoproject/index.js
@@ -12,7 +12,7 @@ function setSession(q, r) {
 }
 
 function attackEnemy(str, enemyId) {
-	console.log("attackEnemy was called. Parameters are " + str + " & " + enemyID);
+	console.log("attackEnemy was called. Parameters are " + str + " & " + enemyId);
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
