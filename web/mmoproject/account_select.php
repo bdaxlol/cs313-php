729ceb984ebb28fac1d5b066ff49770818b48e9b
bdaxlol <dax.guthrie@gmail.com>
2017-10-19 17:13:13 -0700

    fixed sessionResult div id name

--- web/mmoproject/account_select.php
+++ web/mmoproject/account_select.php
@@ -53,7 +53,7 @@ $db = get_db();
 			?>
 			</table>
 
-			<div id="sessionStatus">Waiting for session to update...</div>
+			<div id="sessionResult">Waiting for session to update...</div>
 
 		</article>
 	</body>
